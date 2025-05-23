434211afb03360451b0e7fe4f3c2c36b4a759b59
joaosilva-watanabe <joaorodrigues.silvapedro@gmail.com>
2025-05-23 18:28:32 -0300

    Primeiros arquivos

---
+++ src/login.py
@@ -0,0 +1,12 @@
+from seleniumbase import BaseCase
+class LoginSauceDemo(BaseCase):
+    def login(self):
+        #Abrir o navegador
+        self.open("https://www.saucedemo.com/")
+
+        #Digitando as credenciais
+        self.type("#Username" ,"standard_user")
+        self.type("#Password", "secret_sauce")
+
+        #Interagindo com o botão de login
+        self.click("#login-button")
