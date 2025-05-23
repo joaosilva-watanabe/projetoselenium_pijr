24cee4fa053bd00465a37bd36c8e039692fd0954
Mateus <mat.a.novaes@gmail.com>
2025-05-23 19:45:48 -0300

    Abra a página

--- src/coleta_dados.py
+++ src/coleta_dados.py
@@ -3,4 +3,7 @@ from seleniumbase import BaseCase
 class ColetarDados(BaseCase):
     #Cria função
     def coleta_dados(self):
-        
+        #Abrir a página no navegador
+        self.open("https://www.saucedemo.com/")
+        #Espera o carregamento da página
+        self.wait_for_element(".quote")
