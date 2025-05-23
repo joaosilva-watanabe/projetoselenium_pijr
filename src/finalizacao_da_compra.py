cff1652cdcfb94192766ee3e77a83509cc658a29
joaosilva-watanabe <joaorodrigues.silvapedro@gmail.com>
2025-05-23 19:52:16 -0300

    Documentação do módulo de finalização do pedido

--- src/finalizacao_da_compra.py
+++ src/finalizacao_da_compra.py
@@ -3,12 +3,18 @@ from seleniumbase import BaseCase
 class FinalizarCompra(BaseCase):
     def concluir_compra(self):
 
+        # Buscando as informações da compra
         meio_de_pagamento = self.get_text(".summary_value_label", timeout= None)
         forma_de_entrega = self.get_text("", timeout= None)
         valor_total = self.get_text(".summary_total_label")
+
+        #Clicando no botão para finalizar a compra
         self.click("#finish")
+
+        #Capturando a mensagem de confirmação
         msg_confirmacao = self.get_text(".complete-text")
 
+        #Retornando as informações desejadas
         return print(f"Mensagem de confirmação: '{msg_confirmacao}'\n "
                      f"Valor total: '{valor_total}'\n"
                      f"Meio de pagamento: '{meio_de_pagamento}'\n"
